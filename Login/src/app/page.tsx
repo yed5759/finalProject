67ec37d9f20e3e9c8fad568540b9aab568671242
ידידיה <yed5759@gmail.com>
2025-03-24 17:49:15 +0200

    home page

--- Login/src/app/page.tsx
+++ Login/src/app/page.tsx
@@ -1,50 +1,33 @@
 import "./login.css"
 import Image from "next/image"
 
-export default function Login() {
+export default function Home() {
   return (
       <div>
-        <header id="stripe">
-          <h4>Compose yourself and take notes!</h4>
-          <p className="fs-6 fw-lighter"><small>Login to access the music notes generator</small></p>
-        </header>
-        <div className="container text-center overflow-hidden pt-lg-5">
-          <div className="row d-flex align-items-stretch h-100">
-            <div className="col">
-              <Image src="/notes.png" alt="Notes" width={300} height={200} id="picture" />
-            </div>
-            <div className="col d-flex justify-content-center">
-              <form className="w-50">
-                <div className="row d-flex flex-row g-1 align-items-center">
-                  <div className="col-4">
-                    <label htmlFor="usernameBar" className="col-form-label">Username</label>
-                  </div>
+          <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet"/>
+          <header id="stripe">
+              <h4>Compose yourself and take notes!</h4>
+              <p className="fs-6 fw-lighter"><small>Login to access the music notes generator</small></p>
+          </header>
+          <div className="container text-center overflow-hidden pt-lg-5">
+              <div className="row d-flex align-items-stretch h-100">
                   <div className="col">
-                    <input type="username" id="usernameBar" className="form-control"/>
+                      <Image src="/notes.png" alt="Notes" width={300} height={200} id="picture"/>
                   </div>
-                </div>
-                <div className="row d-flex flex-row g-1 align-items-center">
-                  <div className="col-4">
-                    <label htmlFor="passwordBar" className="col-form-label">Password</label>
+                  <div className="col d-flex justify-content-center flex-column align-items-center text-center mb-1">
+                      <h1><big>Generate Notes with Taking Notes</big></h1>
+                      <h2>start now!</h2>
+                      <button type="button" className="btn btn-dark button mt-3">Log in</button>
                   </div>
-                  <div className="col">
-                    <input type="password" id="passwordBar" className="form-control"/>
-                  </div>
-                </div>
-                <div className="col-5">
-                  <button className="btn btn-primary" type="submit">Sign in</button>
-                </div>
-              </form>
-            </div>
+              </div>
           </div>
-        </div>
-        <div className="border-top border-dark" id="down">
-          <div className="d-flex flex-column align-items-center justify-content-center mt-sm-5">
-            <h5>New User?</h5>
-            <p><small>Sign up and start using Taking Notes</small></p>
-            <button type="button" className="btn btn-dark button">Sign Up</button>
+          <div className="border-top border-dark" id="down">
+              <div className="d-flex flex-column align-items-center justify-content-center mt-sm-5">
+                  <h5>New User?</h5>
+                  <p><small>Sign up and start using Taking Notes</small></p>
+                  <button type="button" className="btn btn-dark button">Sign Up</button>
+              </div>
           </div>
-        </div>
       </div>
   );
 }
