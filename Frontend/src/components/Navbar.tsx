c685ed0a3cc48ce996f20ffd769702a151633675
yed5759 <yed5759@gmail.com>
2025-04-15 20:04:00 +0300

    home page

---
+++ Frontend/src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import '@/app/styles/Navbar.css';
+
+export default function Navbar() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    router.push('/landing'); // או כל עמוד אחר שתרצה להפנות אליו
+  };
+
+  return (
+    <header className="w-full bg-blue-900 text-white flex justify-between items-center">
+      <nav className="navbar">  {/* השתמש במחלקת ה־CSS 'navbar' */}
+        <div className="navbar-title">🎵 Taking Notes</div> {/* השתמש במחלקת ה־CSS 'navbar-title' */}
+        <button onClick={handleLogout} className="navbar-button">Logout</button> {/* השתמש במחלקת ה־CSS 'navbar-button' */}
+      </nav>
+    </header>
+  );
+}
