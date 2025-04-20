b0d2ed7901b743369bc109cd96b489fc5d679891
yed5759 <yed5759@gmail.com>
2025-04-20 13:17:58 +0300

    flask server upload request

--- Backend/routes/home.py
+++ Backend/routes/home.py
@@ -1,13 +1,26 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, redirect
 from Backend.utils.auth import require_auth
+from Backend.utils.inputProcessors import download_audio
 
 home_page = Blueprint('home_page', __name__)
 
 @home_page.route('/home', methods=['POST'])
 @require_auth
-def home():
-    if request.method == 'POST':
-        data = request.form
+def upload():
+    data = request.form.to_dict()
+    uploaded_file = request.files.get("file")
+
+    if uploaded_file and uploaded_file.filename != "":
+        print("Got a file:", uploaded_file.filename)
+    elif data['url']:
+        download_audio(data['url'])
+    else:
+        return "No valid input provided", 400
+
+    return redirect('/Notes')
+
+
+
 
 
 
