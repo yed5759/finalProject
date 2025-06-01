151041ab044e2eb7f3ab3a9188b462629c3b7297
LiorNakar <nakarlior@gmail.com>
2025-06-01 21:09:51 +0300

    fixed mistake i made changing artist to artists

--- Frontend/src/app/MyLibrary/page.tsx
+++ Frontend/src/app/MyLibrary/page.tsx
@@ -6,10 +6,25 @@ import { MdDelete, MdShare } from 'react-icons/md';
 type Song = {
     id: string;
     title: string;
-    artist?: string[];
+    artist?: string;
     notes: string[];
     tags?: string[];
 };
+
+// todo delete
+// songs list
+// const initialSongs = [
+//     { id: '1', title: 'איש עם חץ', artist: 'Hillel', notes: ['Note 1', 'Note 2'], tags: ['pop', '2023'] },
+//     { id: '2', title: 'Song 2', artist: '', notes: ['Note 3', 'Note 4'], tags: ['rock', '2023'] },
+//     { id: '3', title: 'Sad Song', artist: 'Yedidya', notes: ['Note 5'], tags: [] },
+//     { id: '4', title: 'Summer Vibes', artist: 'Dana', notes: ['Note 6', 'Note 7'], tags: ['pop', '2022'] },
+//     { id: '5', title: 'Misty Night', artist: 'Michael', notes: ['Note 8'], tags: ['jazz', '2021'] },
+//     { id: '6', title: 'Deep Waters', artist: 'Sarah', notes: ['Note 9', 'Note 10'], tags: ['rock', '2022'] },
+//     { id: '7', title: 'City Lights', artist: 'Erez', notes: ['Note 11'], tags: ['electronic', '2023'] },
+//     { id: '8', title: 'Old Memories', artist: 'Yaara', notes: ['Note 12', 'Note 13'], tags: ['pop', '2021'] },
+//     { id: '9', title: 'The Sound of Silence', artist: 'Matan', notes: ['Note 14'], tags: ['indie', '2023'] },
+//     { id: '10', title: 'Shadows', artist: 'Noa', notes: ['Note 15', 'Note 16'], tags: ['rock', '2023'] },
+// ];
 
 export default function MyLibrary() {
     // songs list
@@ -22,8 +37,8 @@ export default function MyLibrary() {
     // Filter songs based on search query
     const filteredSongs = songs.filter((song) =>
         song.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        (song.artist && song.artist.some(artist => artist.toLowerCase().includes(searchQuery.toLowerCase()))) ||
-        (song.tags && song.tags.some(tag => tag.toLowerCase().includes(searchQuery.toLowerCase())))
+        (song.artist && song.artist.toLowerCase().includes(searchQuery.toLowerCase())) ||
+        (song.tags && song.tags.some(tag => tag.toLowerCase().includes(searchQuery.toLowerCase()))) // if search query matches any tag
     );
 
     // Handle deleting a song
